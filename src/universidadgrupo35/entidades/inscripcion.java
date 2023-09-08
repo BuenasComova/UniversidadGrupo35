1a7a33341f5f6a767d4953b88af2e7920f63c666
DiegoNicolasFlores <icaro.com@hotmail.com>
2023-09-08 18:54:51 -0300

    acctualizacion1
    se rellenaron las clases inscripcion y materia

--- src/universidadgrupo35/entidades/inscripcion.java
+++ src/universidadgrupo35/entidades/inscripcion.java
@@ -10,5 +10,64 @@ package universidadgrupo35.entidades;
  * @author Hp
  */
 public class inscripcion {
+    private int idInscripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private double nota;
+
+    public inscripcion() {
+    }
+
+    public inscripcion(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public inscripcion(int idInscripcion, Alumno alumno, Materia materia, double nota) {
+        this.idInscripcion = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        String insc = idInscripcion+" "+alumno.getApellido()+", "+alumno.getNombre()+" "+materia.getNombre();
+        return insc;
+    }
+    
     
 }
