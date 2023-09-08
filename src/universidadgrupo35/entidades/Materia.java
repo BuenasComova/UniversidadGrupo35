dfdfa38da2db144daf7b1219e6500ba45b94eeea
DiegoNicolasFlores <icaro.com@hotmail.com>
2023-09-08 18:24:21 -0300

    Initial commit

---
+++ src/universidadgrupo35/entidades/Materia.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadgrupo35.entidades;
+
+/**
+ *
+ * @author Hp
+ */
+public class Materia {
+  
+    private int idMateria;
+    private String nombre;
+    private int anio;
+    private boolean activo;
+
+    public Materia() {
+    }
+    
+   
+
+    
+    }
+    
+
